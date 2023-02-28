4523c5d2554a7ac1be9f5e0f846bf39f531693ab
rithudivakar <111083162+rithudivakar@users.noreply.github.com>
2023-02-28 13:52:00 +0530

    Add files via upload

--- service/service.py
+++ service/service.py
@@ -10,6 +10,7 @@ import numpy as np
 import gensim.downloader as api
 import string
 import pickle
+import math
 
 
 
@@ -29,17 +30,43 @@ class AnomalyDetectionService:
         text = text.translate(str.maketrans("", "", string.punctuation))
         # Split the input text into individual words
         words = text.lower().split()
-        
+
         # Calculate the vector representation of the input text by averaging the vectors of the individual words
         vectors = []
         for word in words:
             if word in data:
                 vectors.append(data[word])
+        # vectors.add(thisLine.split(";"))
+        # String[][] array = new String[vectors.size()][0]
+        # lines.toArray(array)
+
         if len(vectors) > 0:
             vector = sum(vectors) / len(vectors)
+    
         else:
             vector = np.zeros(25)
+            # vectors = np.reshape(vectors.shape[0], -1)
         return vector
+
+    # def toBinary(self, a):
+    #     l,m=[],[]
+    #     for i in a:
+    #         l.append(ord(i))
+    #     for i in l:
+    #         m.append(int(bin(i)[2:]))
+    #     return m
+
+    # def toBinary(self, a):
+    #     l = [ord(i) for i in a]
+    #     m = np.array([int(bin(i)[2:]) for i in l])
+    #     returdef toBinary(self, s):
+    
+    
+    # def toBinary(self, s):
+    #     encoded = []
+    #     for c in s:
+    #         encoded.append(ord(c))
+    #     return encoded
 
 
 
@@ -56,8 +83,22 @@ class AnomalyDetectionService:
 
 
 
+    # def detect_anomalies(self) -> List[Data]:
+    #     X = [d.record for d in self.repository.get_data()]
+    #     # X = X.reshape(X.shape[0], -1)
+    #     dataneighbors = NearestNeighbors(n_neighbors=self.k)
+    #     dataneighbors.fit(X)
+    #     distances, indices = dataneighbors.kneighbors(X)
+    #     anomalies = []
+    #     for i in range(len(self.repository.get_data())):
+    #         if distances[i][-1] > 10:
+    #             self.repository.get_data()[i].is_anomaly = True
+    #             anomalies.append(self.repository.get_data()[i])
+    #     return anomalies
+
     def detect_anomalies(self) -> List[Data]:
         X = [d.record for d in self.repository.get_data()]
+        X = np.asarray(X)
         dataneighbors = NearestNeighbors(n_neighbors=self.k)
         dataneighbors.fit(X)
         distances, indices = dataneighbors.kneighbors(X)
